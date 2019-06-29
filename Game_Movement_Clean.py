4d4cab6007d59cc0afdf326c0154ae7061c01fbf
henriquecscode <henriquecostasousa.hcs@gmail.com>
2019-06-29 17:45:34 +0100

    refactored the code with classes

--- Game_Movement_Clean.py
+++ Game_Movement_Clean.py
@@ -14,11 +14,15 @@ walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.im
 
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 
+
 bg = pygame.image.load('bg.jpg') # Background
 
 char = pygame.image.load('standing.png') # character not moving or jumping
 
 clock = pygame.time.Clock()
+
+class Object(object): #Used to make simple objects
+    pass
 
 class spritesheet(object):
     def __init__(self, filename):
@@ -48,6 +52,26 @@ class spritesheet(object):
         tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
                 for x in range(image_count)]
         return self.images_at(tups, colorkey)
+    
+goblin_animation = Object()
+setattr(goblin_animation, 'walkRight', [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'), pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'), pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')])
+setattr(goblin_animation, 'walkLeft', [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'), pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'), pygame.image.load('L8E.png'), pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')])
+
+
+bat_ss = spritesheet('rat and bat spritesheet calciumtrice.png')
+bat_animation = Object()
+bat_coordinates = []
+for x in range(10):
+    bat_coordinates.append((x*32, 160,32,32))
+setattr(bat_animation, 'walkRight', bat_ss.images_at(bat_coordinates, colorkey=(255,255,255)))
+
+
+rat_ss = spritesheet('rat and bat spritesheet calciumtrice.png')
+rat_animation = Object()
+rat_coordinates = []
+for x in range(10):
+    rat_coordinates.append((x*32, 96,32,32))
+setattr(rat_animation, 'walkRight', rat_ss.images_at(rat_coordinates, colorkey=(255,255,255)))
 
 class player(object):
     def __init__(self, x, y, width, height):
@@ -94,17 +118,32 @@ class projectile(object):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 class enemy(object):
-    walkRight = [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'), pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'), pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')]
-    walkLeft = [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'), pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'), pygame.image.load('L8E.png'), pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')]
-
-    def __init__(self, x, y, width, height, end):
-        self.x = x 
-        self.y = y
+    def __init__(self, images, x, y, width, height):
+        self.images = images
         self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.walkCount = 0
+
+    def draw(self, win):
+        self.move()
+        if(self.vel > 0):
+            win.blit(self.images.walkRight[self.walkCount])
+        elif(self.vel < 0):
+            win.blit(self.images.walkLeft[self.walkCount])
+        self.walkCount = (self.walkCount + 1) % len(self.images.walkRight)
+
+    def move(self, x, y):
+        return
+
+class enemy_ground(enemy):
+
+    def __init__(self, images, x, y, width, height, end):
+        super(enemy_ground, self).__init__(images, x, y, width, height)
         self.height = height
         self.end = end
         self.path = [self.x, self.end]
-        self.walkCount = 0
         self.vel = 3
     
     def draw(self, win):
@@ -112,10 +151,10 @@ class enemy(object):
         if self.walkCount >= 33:        #33
             self.walkCount = 0
         if self.vel > 0:
-            win.blit(self.walkRight[self.walkCount // 3], (self.x,self.y))
+            win.blit(self.images.walkRight[self.walkCount // 3], (self.x,self.y))
             self.walkCount += 1
         else:
-            win.blit(self.walkLeft[self.walkCount //3], (self.x, self.y))
+            win.blit(self.images.walkLeft[self.walkCount //3], (self.x, self.y))
             self.walkCount += 1
 
         pass
@@ -136,20 +175,27 @@ class enemy(object):
 
         pass
 
+class goblin(enemy_ground):
+    #goblin_animation.walkRight = [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'), pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'), pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')]
+    #goblin_animation.walkLeft = [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'), pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'), pygame.image.load('L8E.png'), pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')]
 
-ss = spritesheet('rat and bat spritesheet calciumtrice.png')
-class enemy2(object):
-    coordinates = []
-    walkRight = []
-    for x in range(10):
-        coordinates.append((x*32, 160,32,32))
-    print(coordinates)
-    walkRight = ss.images_at(coordinates, colorkey=(255,255,255))
-    def __init__(self, to_attack):
+    
+    def __init__(self, x, y, end):
+        super(goblin, self).__init__(goblin_animation, x, y, 64, 64, end)
+        self.end = end
+
+class rat(enemy_ground):
+
+    def __init(self, x, y, end):
+        super(rat, self).__init__(rat_animation, x, y, 64, 64, end)
+        self.end = end
+
+class enemy_flying(enemy):
+    def __init__(self, images, to_attack, x , witdh, height):
+        super(enemy_flying, self).__init__(images, x, 10, width, height)
         self.speed = 5
         self.x = random.randint(0, width)
         self.goal_x = random.randint(0, width)
-        self.y = 10
         self.attacking = 1
         self.to_attack = to_attack
         self.walkCount = 0
@@ -158,7 +204,7 @@ class enemy2(object):
         self.move(self.to_attack.x, self.to_attack.y +10)
 ##        print(self.x, self.y)
 ##        print(self.attacking)
-        win.blit(self.walkRight[self.walkCount % 10], (self.x, self.y))
+        win.blit(self.images.walkRight[self.walkCount % 10], (self.x, self.y))
         self.walkCount = (self.walkCount + 1) % 10
 
     def move(self, x, y): #x and y of the player
@@ -185,23 +231,27 @@ class enemy2(object):
         y_vel = math.sin(angle) * self.speed
         return [x_vel, y_vel]
 
+class bat(enemy_flying):
+
+    def __init__(self, to_attack):
+        super(bat, self).__init__(bat_animation, to_attack, x, 32, 32)
 
 def redrawGameWindow():
     global walkCount        #defines the variable in the whole code
     win.blit(bg, (0,0))
     man.draw(win)          # Player
-    goblin.draw(win)
-    bat.draw(win)
+    goblin1.draw(win)
+    bat1.draw(win)
     for bullet in bullets:
         bullet.draw(win) 
 
     pygame.display.update()
 
 #Main Loop
-
 man = player(300, 410, 64, 64)  # Character Specs (x,y,width,height)
-goblin = enemy(100, 410, 64, 64, 450)
-bat = enemy2(man)
+goblin1 = goblin(100, 410, 450)
+#goblin = enemy(100, 410, 64, 64, 450)
+bat1 = bat(man)
 bullets =  []
 run = True
 
